277ca379eb5f3dba8ae9d77b9cd89ef334046f7e
SyedAleezHassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-01-01 14:15:13 +0500

    Create vowels.dart

---
+++ vowels.dart
@@ -0,0 +1,8 @@
+void main() {
+  var val = "y";
+  if (val == "a" || val == "e" || val == "i" || val == "o" || val == "u") {
+    print("The character is vowel");
+  } else {
+    print(" This is not a vowel");
+  }
+}
