91bcfbe33eae34068c24cd2bfd4bedf76cec3842
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-02-11 14:45:32 +0500

    Create b.dart

---
+++ b.dart
@@ -0,0 +1,19 @@
+void printLetterB() {
+  List<String> letterB = [
+    '*****',
+    '*    *',
+    '*    *',
+    '*****',
+    '*    *',
+    '*    *',
+    '*****',
+  ];
+
+  for (String line in letterB) {
+    print(line);
+  }
+}
+
+void main() {
+  printLetterB();
+}
