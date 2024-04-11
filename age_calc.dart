c76f1d3e65a5a3d6d7544782813c4dd93bc434ad
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-04-11 16:00:39 +0500

    Create age_calc.dart

---
+++ age_calc.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  print('Enter your birthdate (YYYY-MM-DD):');
+  String input = stdin.readLineSync()!;
+  DateTime birthDate = DateTime.parse(input);
+
+  DateTime now = DateTime.now();
+
+  int age = now.year - birthDate.year;
+  if (now.month < birthDate.month ||
+      (now.month == birthDate.month && now.day < birthDate.day)) {
+    age--;
+  }
+
+  print('Your age is: $age');
+}
